7df081610a77fc9e219a1cbf5c95ef43edafad1e
John Castillo <johnfra797@hotmail.com>
2020-07-05 16:55:38 -0500

    initial commit

---
+++ API_MAS/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_MAS.DTO;
+using Datos.Base.Definiciones;
+using Datoss.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace API_MAS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ILogger<EmployeeController> _logger;
+        private readonly IMASRepository _IMASRepository;
+        public EmployeeController(ILogger<EmployeeController> logger, IMASRepository mASRepository)
+        {
+            _logger = logger;
+            _IMASRepository = mASRepository;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "API MAS";
+        }
+
+        [HttpPost]
+        public string Post(SearchDTO searchDTO)
+        {
+            var ListResult= _IMASRepository.Get(searchDTO.searchValue.ToLower());
+            string result = JsonConvert.SerializeObject(ListResult);
+            return result;
+        }
+    }
+}
---
+++ API_MAS/DTO/SearchDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_MAS.DTO
+{
+    public class SearchDTO
+    {
+        public string searchValue { get; set; }
+    }
+}
